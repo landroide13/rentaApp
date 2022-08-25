9651e74f8c19701a2a1b0bbae3e45bb99b32cb03
Luis Andrade Reuss <reysh_2@yahoo.com>
2022-08-26 10:32:54 +1200

    home and requirement

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    #question_list = Question.objects.all()
+    #context = {'questions': question_list}
+    return render(request, 'home.html')
